d9c445b5753651bb6b42bd84b851144d28a3cd39
ckbang <ckbang7@gmail.com>
2020-08-15 21:40:01 +0900

    SteamVR interaction stuff

---
+++ Samples/Examples/Interactable/Installers/IF_VR_Steam_Example_Interactable_Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace InterVR.IF.VR.Plugin.Steam.Examples.Interactable.Installer
+{
+    [CreateAssetMenu(fileName = "IF_VR_Steam_Example_Interactable_Settings", menuName = "InterVR/IF/Plugin/VR/Steam/Example/Interactable/Settings")]
+    public class IF_VR_Steam_Example_Interactable_Installer : ScriptableObjectInstaller<IF_VR_Steam_Example_Interactable_Installer>
+    {
+#pragma warning disable 0649
+        [SerializeField]
+        Settings settings;
+#pragma warning restore 0649
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(settings).IfNotBound();
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public string Name = "IF Steam VR Plugin Example Interactable Installer";
+        }
+    }
+}
---
+++ Samples/Examples/Throwing/Installers/IF_VR_Steam_Example_Throwing_Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace InterVR.IF.VR.Plugin.Steam.Examples.Throwing.Installer
+{
+    [CreateAssetMenu(fileName = "IF_VR_Steam_Example_Throwing_Settings", menuName = "InterVR/IF/Plugin/VR/Steam/Example/Throwing/Settings")]
+    public class IF_VR_Steam_Example_Throwing_Installer : ScriptableObjectInstaller<IF_VR_Steam_Example_Throwing_Installer>
+    {
+#pragma warning disable 0649
+        [SerializeField]
+        Settings settings;
+#pragma warning restore 0649
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(settings).IfNotBound();
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public string Name = "IF Steam VR Plugin Example Throwing Installer";
+        }
+    }
+}
