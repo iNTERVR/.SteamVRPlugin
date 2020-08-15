dfd30acdba186b510b44018402a0bb94e8df21fb
ckbang <ckbang7@gmail.com>
2020-08-15 16:37:20 +0900

    Release 0.1.0

---
+++ Runtime/Installers/IF_VR_Steam_MonoInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Zenject;
+
+namespace InterVR.IF.VR.Plugin.Steam.Installer
+{
+    public class IF_VR_Steam_MonoInstaller : MonoInstaller<IF_VR_Steam_MonoInstaller>
+    {
+        public List<ScriptableObjectInstaller> settings;
+
+        public override void InstallBindings()
+        {
+            var settingsInstaller = settings.Cast<IInstaller>();
+            foreach (var installer in settingsInstaller)
+            {
+                Container.Inject(installer);
+                installer.InstallBindings();
+            }
+        }
+    }
+}
